45a29bb36da051baed1c1975e54c794f70267e71
Billy Lowry <billyn.lowry@gmail.com>
2023-05-30 16:19:34 -0400

    completed lab

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,26 @@
 #!/usr/bin/env python3
-
+#using a while loop that outputs numbers starting at 10 and counting down to 1. 
+#After reaching 1, print out "Happy New Year!
 def happy_new_year():
-    # code goes here!
-    pass
+    countdown = 10 
+    while countdown in range (10, 0, -1):
+        print(countdown)
+        countdown -=1 
+        print("Happy New Year!")
 
+#takes one argument, a list of integers and returns the list of squared elements.
 def square_integers(int_list):
-    # code goes here!
-    pass
+    return [i ** 2 for i in int_list]
+
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    for i in range(1,101): #prints number 1-100
+        if not i % 15: 
+            print("FizzBuzz") 
+        elif not i % 5: # 
+            print("Buzz")
+        elif not i % 3: #mutiples of 3 print out fizz 
+            print("Fizz")
+        else:
+            print(i)
+
